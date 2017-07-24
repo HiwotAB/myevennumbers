3c4f96b673679a1a51b853ce7608a8bd31c86791
student <student@GBTC440J08LX.montgomerycollege.edu>
2017-07-24 16:58:07 -0400

    Even Numbers from 1 to 100

---
+++ EvenNumber.java
@@ -0,0 +1,20 @@
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Even Number from 1 to 100:");
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+"is an even number");	
+			}
+			else
+				System.out.println(i);
+		}
+
+	}
+
+}
